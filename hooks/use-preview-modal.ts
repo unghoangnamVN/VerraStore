1861895867cc5cef9b584dbae7e9eb3f3f6c348d
unghoangnam <unghoangnam389@gmail.com>
2024-07-30 17:10:25 +0700

    fixed hook used preview

--- hooks/use-preview-modal.ts
+++ hooks/use-preview-modal.ts
@@ -1,20 +1,21 @@
-import { create } from "zustand";
+import { Product } from '@/types'
+import { create } from 'zustand'
 
 interface PreviewModalStore {
-  isOpen: boolean;
-  type?: "food" | "drink";
-  data?: Product;
-  onOpen: (type: "food" | "drink", data: Product) => void;
-  onClose: () => void;
+    isOpen: boolean
+    type?: 'food' | 'drink'
+    data?: Product
+    onOpen: (type: 'food' | 'drink', data: Product) => void
+    onClose: () => void
 }
 
 const usePreviewModal = create<PreviewModalStore>((set) => ({
-  isOpen: false,
-  type: undefined,
-  data: undefined,
-  onOpen: (type: "food" | "drink", data: Product) =>
-    set({ isOpen: true, type, data }),
-  onClose: () => set({ isOpen: false }),
-}));
+    isOpen: false,
+    type: undefined,
+    data: undefined,
+    onOpen: (type: 'food' | 'drink', data: Product) =>
+        set({ isOpen: true, type, data }),
+    onClose: () => set({ isOpen: false }),
+}))
 
-export default usePreviewModal;
+export default usePreviewModal
